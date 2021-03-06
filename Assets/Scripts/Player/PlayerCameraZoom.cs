909f1f6b7f6b5c1914fc8e8858442934667ad733
PAzter1101 <PAzter1101@gmail.com>
2021-03-07 00:50:41 +0300

    -Fixed zoom function from PlayerCamera.

---
+++ Assets/Scripts/Player/PlayerCameraZoom.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Cinemachine;
+
+public class PlayerCameraZoom : MonoBehaviour
+{
+    private CinemachineTransposer CinemachineTransposer;
+
+    void Start()
+    {
+        CinemachineTransposer = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
+    }
+
+    void Update()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            CinemachineTransposer.m_FollowOffset.y += (Input.GetAxis("Mouse ScrollWheel") * -5);
+        }
+    }
+}
