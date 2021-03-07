aedc4bd8140dc96f64240a4d2d8448e79e78da2d
PAzter1101 <PAzter1101@gmail.com>
2021-03-07 09:03:24 +0300

    -Add limit for zooming function.

--- Assets/Scripts/Player/PlayerCameraZoom.cs
+++ Assets/Scripts/Player/PlayerCameraZoom.cs
@@ -3,18 +3,24 @@ using Cinemachine;
 
 public class PlayerCameraZoom : MonoBehaviour
 {
-    private CinemachineTransposer CinemachineTransposer;
+    [SerializeField]
+    private float minZoom;
+    [SerializeField]
+    private float maxZoom;
+
+    private CinemachineTransposer cinemachineTransposer;
 
     void Start()
     {
-        CinemachineTransposer = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
+        cinemachineTransposer = gameObject.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<CinemachineTransposer>();
     }
 
     void Update()
     {
         if(Input.GetAxis("Mouse ScrollWheel") != 0)
         {
-            CinemachineTransposer.m_FollowOffset.y += (Input.GetAxis("Mouse ScrollWheel") * -5);
+            cinemachineTransposer.m_FollowOffset.y += (Input.GetAxis("Mouse ScrollWheel") * -5);
+            cinemachineTransposer.m_FollowOffset.y = Mathf.Clamp(cinemachineTransposer.m_FollowOffset.y, minZoom, maxZoom);
         }
     }
 }
